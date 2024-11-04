2e3637ad0b131b4076265b24ba73b932f0cd926f
web-coder <yasher18@bk.ru>
2024-11-04 16:41:10 +0300

    изучал DOM методы js

--- assassins/app.js
+++ assassins/app.js
@@ -34,3 +34,53 @@ text3.addEventListener("click", function() {
         alert("Ну тут хрен пойми что")
     }
 })
+
+let qwerty1 = text1.getAttribute("qwerty")
+console.log(qwerty1)
+
+let text_array = ["text1", "text2", "text3"]
+let qwerty_array = []
+
+for (let i = 0; i < text_array.length; i++) {
+    qwerty_array.push(document.getElementById(text_array[i]).getAttribute("qwerty"))
+}
+
+console.log(qwerty_array)
+
+console.log(document.children[0].children[0].getElementsByTagName("title")[0])
+console.log(document.getElementsByTagName("title")[0])
+
+console.log(document.childElementCount)
+
+// дальше идет classList и его методы add и remove тут и так понятно что add добавляет а remove удаляет класс у эдемнта
+
+let text4 = document.getElementById("text4")
+let text4_status = false
+text4.classList.add("copy-text")
+
+// здесь создам функцию которая на 100 милисекунд поставит один текст а потом другой 
+
+function setHtml() {
+    setTimeout(function() {
+        text4.textContent = "(c)2024 February >>> Altair Ibn La Ahad"
+    }, 400)
+    text4.textContent = "Altair the best of assassins creed"
+}
+
+text4.addEventListener("click", function() {
+    text4_status =! text4_status
+
+    if (text4_status == true) {
+        text4.classList.add("copy2")
+        text4.textContent = "(c)2024 February >>> Altair Ibn La Ahad"
+    } else {
+        text4.classList.remove("copy2")
+        setHtml()
+    }
+
+})
+
+console.log("-----------------------")
+
+console.log(document.querySelectorAll(".card-text"))
+console.log(document.querySelector(".card-text").innerHTML)
