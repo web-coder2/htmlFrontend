6062a36afaedc2931be229bf4c926bfa3e6495b6
web-coder <yasher18@bk.ru>
2024-11-20 15:42:35 +0300

    fix bug in lab2.py

--- pythonMath/lab2.py
+++ pythonMath/lab2.py
@@ -5,12 +5,13 @@ matrixA = np.array([ [9.9, -1.5, 2.6], [2.5, 5.8, -0.5], [0.7, 0.4, 7.1] ])
 matrixB = np.array([0, 3.5, -1.3])
 tolerance = 10**(-12)
 numIterations = 1000
+x0 = ([0, 0, 0])
 
 
 # Метод Гаусса-Жордана
 try:
     x = linalg.solve(matrixA, matrixB)
-    print("Решение системы уравнений: " + str(x))
+    print("Решение системы уравнений методом scipy.linalg.solve: " + str(x))
 except linalg.LinAlgError:
     print("Система уравнений не имеет решений")
 
@@ -30,8 +31,8 @@ def simple_iteration(A, b, x0, tolerance, max_iterations):
     for i in range(max_iterations):
         x_new = D_inv @ (b - R @ x)
         if np.linalg.norm(x_new - x) < tolerance:
-            return x_new, f"Метод сошелся за {i+1} итераций."
+            return x_new, f"Метод сошелся за {i+1} итераций.", "метод простых итераций"
         x = x_new
     return None 
 
-print(simple_iteration(matrixA, matrixB, tolerance, numIterations))
+print(simple_iteration(matrixA, matrixB, x0, tolerance, numIterations))
